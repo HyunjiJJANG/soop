819a20ac8550e2fc74e3168661ceca2a0cfcaeb8
HJ <hhj9577@naver.com>
2023-07-20 10:47:37 +0900

    commit

---
+++ soop/src/main/java/kr/co/jhta/soop/SoopApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.soop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoopApplication.class, args);
+	}
+
+}
---
+++ soop/src/main/java/kr/co/jhta/soop/test/test.java
@@ -0,0 +1,7 @@
+package kr.co.jhta.soop.test;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
