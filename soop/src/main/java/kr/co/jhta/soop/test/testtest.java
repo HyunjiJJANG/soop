7883e86d9faa42b489aae108bbd872a52b387980
KBW <rlaquddnr16@naver.com>
2023-07-20 11:51:16 +0900

    testtest

--- soop/src/main/java/kr/co/jhta/soop/test/testtest.java
+++ soop/src/main/java/kr/co/jhta/soop/test/testtest.java
@@ -1,7 +1,10 @@
 package kr.co.jhta.soop.test;
 
-public class test {
+public class testtest {
+
 	public static void main(String[] args) {
-		System.out.println("kbw test");
+		System.out.println("testtesttesttest");
+
 	}
+
 }
