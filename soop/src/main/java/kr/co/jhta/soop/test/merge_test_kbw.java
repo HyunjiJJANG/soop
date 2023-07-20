a32b1dde88491bfe415d33346b903b91b354f9e2
KBW <rlaquddnr16@naver.com>
2023-07-20 11:32:42 +0900

    merge_test_kbw

--- soop/src/main/java/kr/co/jhta/soop/test/merge_test_kbw.java
+++ soop/src/main/java/kr/co/jhta/soop/test/merge_test_kbw.java
@@ -1,9 +1,10 @@
 package kr.co.jhta.soop.test;
 
-public class mergetest_kbw {
+public class merge_test_kbw {
 
 	public static void main(String[] args) {
 		System.out.println("merge_test_kbw");
+
 	}
 
 }
