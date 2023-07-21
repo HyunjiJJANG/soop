d24feddd747563f8f9912efacc9201755544432a
HJ <hhj9577@naver.com>
2023-07-21 17:45:38 +0900

    20230721 - 회원가입시 데이터 저장되고 이메일 중복 확인까지 완료, 이메일 인증 하던 중 커밋

---
+++ soop/src/main/java/kr/co/jhta/soop/service/EmailConfig.java
@@ -0,0 +1,8 @@
+package kr.co.jhta.soop.service;
+
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:application.properties")
+public class EmailConfig {
+
+}
--- soop/src/main/java/kr/co/jhta/soop/service/EmailUtil.java
+++
@@ -1,9 +0,0 @@
-package kr.co.jhta.soop.service;
-
-
-
-public interface EmailUtil {
-	
-	void sendEmail(String toAddress, String subject, String body);
-
-}
